1bde33e2c81ebfaeec78bcdc8709810eb4d47345
li.li <mark.li210812@gmail.com>
2022-06-19 23:44:23 +0800

    syntax highlight and img style

--- app/routes/detail/$number.jsx
+++ app/routes/detail/$number.jsx
@@ -1,18 +1,47 @@
-import { useLoaderData } from "@remix-run/react"
-import { getBlogDetail } from "~/github.server"
-import { format } from '~/utils/date'
+import { useLoaderData } from "@remix-run/react";
+import { useEffect } from "react";
+// import hljs from 'highlight.js'
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import css from "highlight.js/lib/languages/css";
+import html from "highlight.js/lib/languages/xml";
+import highlightStyle from "highlight.js/styles/night-owl.css";
+
+import { getBlogDetail } from "~/github.server";
+import { format } from "~/utils/date";
+
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("css", css);
+hljs.registerLanguage("html", html);
+
+export const links = () => [
+  {
+    rel: "stylesheet",
+    href: highlightStyle,
+  },
+];
 
 export const loader = ({ params }) => {
-  return getBlogDetail(params.number)
-}
+  return getBlogDetail(params.number);
+};
 
 export default function Detail() {
   const data = useLoaderData();
-  console.log(data);
+  // console.log(data);
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
 
-  return <article>
-    <h3>{data.title}</h3>
-    <p>{format(data.created_at)} <a href={data.html_url} target="_blank" rel="noreferrer">source</a></p>
-    <main dangerouslySetInnerHTML={{__html: data.html}} />
-  </article>
+  return (
+    <article>
+      <h1>{data.title}</h1>
+      <p>
+        {format(data.created_at)}{" "}
+        <a href={data.html_url} target="_blank" rel="noreferrer">
+          source
+        </a>
+      </p>
+      <main dangerouslySetInnerHTML={{ __html: data.html }} />
+    </article>
+  );
 }
