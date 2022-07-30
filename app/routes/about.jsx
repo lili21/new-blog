6a38eb26bf8b01df93aead86a83f3b8c153bccab
li.li <mark.li210812@gmail.com>
2022-07-30 19:18:36 +0800

    feat: update aboutme

--- app/routes/about.jsx
+++ app/routes/about.jsx
@@ -1,8 +1,15 @@
 export default function About() {
   return (
     <div>
-      前端开发，目前就职于字节跳动。
-      <a href="mailto:mark.li210812@gmail.com">联系我</a>
+      前端开发，目前就职于字节跳动
+      <ui>
+        <li>
+          <a href="https://github.com/lili21">Github</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/mark_li21">Twitter</a>
+        </li>
+      </ui>
     </div>
   );
 }
