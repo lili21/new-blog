98f7affce2465c28378007e57821fd5f3d4284aa
li.li <mark.li210812@gmail.com>
2022-10-19 21:24:41 +0800

    test: mdx-bundler and shiki

--- remix.config.js
+++ remix.config.js
@@ -10,5 +10,5 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
-  serverDependenciesToBundle: [/^mdx-bundler.*/],
+  serverDependenciesToBundle: [/mdx-bundler/, /shiki/],
 };
