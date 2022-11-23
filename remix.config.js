51038711255df5818b3acc923dc1eb16e72cd26a
li.li <mark.li210812@gmail.com>
2022-11-23 21:28:30 +0800

    vercel

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,8 @@ const theme = require("shiki/themes/nord.json");
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
