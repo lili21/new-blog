472e9bea62a04ab252681ed6a30a2050ae825b7d
li.li <mark.li210812@gmail.com>
2022-11-23 10:15:05 +0800

    feat: utterance

--- app/routes/detail.jsx
+++ app/routes/detail.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "@remix-run/react";
 import codeHideStyle from "@code-hike/mdx/dist/index.css";
+import { useEffect } from "react";
 
 export const links = () => [
   {
@@ -17,5 +18,15 @@ export const headers = () => {
 };
 
 export default function Detail() {
+  useEffect(() => {
+    // fixme - StrictMode下会执行两次
+    const s = document.createElement("script");
+    s.src = "https://utteranc.es/client.js";
+    s.setAttribute("repo", "lili21/blog-comments");
+    s.setAttribute("theme", "github-light");
+    s.setAttribute("crossorigin", "anonymous");
+    s.setAttribute("issue-term", "pathname");
+    document.body.appendChild(s);
+  }, []);
   return <Outlet />;
 }
