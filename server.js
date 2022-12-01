63e2bc08ff7e2158dd576f4b0c8472512d0a5ebd
li.li <mark.li210812@gmail.com>
2022-12-01 12:08:20 +0800

    feat: metronome

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-import { createRequestHandler } from "@remix-run/vercel";
+import {
+  registerMetronome,
+  createMetronomeGetLoadContext,
+} from "@metronome-sh/vercel";
 import * as build from "@remix-run/dev/server-build";
+import { createRequestHandler } from "@remix-run/vercel";
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+const buildWithMetronome = registerMetronome(build);
+
+const metronomeGetLoadContext = createMetronomeGetLoadContext(
+  buildWithMetronome,
+  { config: require("./metronome.config.js") }
+);
+
+export default createRequestHandler({
+  build: buildWithMetronome,
+  getLoadContext: metronomeGetLoadContext,
+  mode: process.env.NODE_ENV,
+});
