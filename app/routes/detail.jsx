107c3af228700bd5c459ceb3a88f3c4c2badf1ae
li.li <mark.li210812@gmail.com>
2022-12-31 20:45:54 +0800

    fix: remove utterance temp

--- app/routes/detail.jsx
+++ app/routes/detail.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "@remix-run/react";
 import codeHideStyle from "@code-hike/mdx/dist/index.css";
-import { useEffect, useRef } from "react";
 
 export const links = () => [
   {
@@ -18,20 +17,21 @@ export const headers = () => {
 };
 
 export default function Detail() {
-  const utterancLoaded = useRef(false);
-  useEffect(() => {
-    if (utterancLoaded.current) {
-      return;
-    }
-    // fixme - StrictMode下会执行两次
-    const s = document.createElement("script");
-    s.src = "https://utteranc.es/client.js";
-    s.setAttribute("repo", "lili21/blog-comments");
-    s.setAttribute("theme", "github-light");
-    s.setAttribute("crossorigin", "anonymous");
-    s.setAttribute("issue-term", "pathname");
-    document.body.appendChild(s);
-    utterancLoaded.current = true;
-  }, []);
+  //const utterancLoaded = useRef(false);
+  //useEffect(() => {
+  //  console.log(utterancLoaded.current, "useRef");
+  //  if (utterancLoaded.current) {
+  //    return;
+  //  }
+  //  // fixme - StrictMode下会执行两次
+  //  const s = document.createElement("script");
+  //  s.src = "https://utteranc.es/client.js";
+  //  s.setAttribute("repo", "lili21/blog-comments");
+  //  s.setAttribute("theme", "github-light");
+  //  s.setAttribute("crossorigin", "anonymous");
+  //  s.setAttribute("issue-term", "pathname");
+  //  document.body.appendChild(s);
+  //  utterancLoaded.current = true;
+  //}, []);
   return <Outlet />;
 }
