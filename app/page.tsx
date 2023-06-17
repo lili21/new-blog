d02fdf5e9415b5c8568a049cc6e671ba5a6d4196
li.li <mark.li210812@gmail.com>
2023-06-17 15:29:16 +0800

    style: 首页样式更新

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,56 @@ import "./page.css";
 import { allPosts } from "contentlayer/generated";
 
 export default async function Home() {
-  const posts = allPosts.sort((a, b) => {
-    return compareDesc(new Date(a.date), new Date(b.date));
-  });
+  // const posts = allPosts.sort((a, b) => {
+  //   return compareDesc(new Date(a.date), new Date(b.date));
+  // });
+
+  const groupedPosts = allPosts.reduce(
+    (acc: Record<number, typeof allPosts>, post) => {
+      const year = new Date(post.date).getFullYear();
+      if (!acc[year]) {
+        acc[year] = [];
+      }
+      acc[year].push(post);
+      return acc;
+    },
+    {}
+  );
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      {/* <h2>文章</h2> */}
+      <h2 style={{ marginBottom: "26px" }}>文章</h2>
       <ol>
-        {posts.map((d) => (
-          <Article key={d._id} title={d.title} date={d.date} url={d.url} />
-        ))}
+        {Object.keys(groupedPosts)
+          .sort((a, b) => Number(b) - Number(a))
+          .map((year) => {
+            return (
+              <li style={{ marginBottom: "10px" }} key={year}>
+                <p
+                  style={{
+                    paddingTop: "30px",
+                    marginBottom: "40px",
+                    fontSize: "18px",
+                  }}
+                >
+                  {year}
+                </p>
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                  {groupedPosts[+year]
+                    .sort((a, b) =>
+                      compareDesc(new Date(a.date), new Date(b.date))
+                    )
+                    .map((d) => (
+                      <Article
+                        key={d._id}
+                        title={d.title}
+                        date={d.date}
+                        url={d.url}
+                      />
+                    ))}
+                </ul>
+              </li>
+            );
+          })}
       </ol>
     </div>
   );
@@ -31,9 +71,27 @@ function Article({
 }) {
   return (
     <li>
-      <Link href={url}>{title}</Link>
-      <br />
-      <em>{format(new Date(date), "yyyy-MM-dd")}</em>
+      <Link href={url} style={{ display: "block" }}>
+        <div
+          style={{
+            display: "flex",
+            fontSize: "18px",
+            height: "60px",
+            alignItems: "center",
+          }}
+        >
+          <p
+            style={{
+              fontSize: "14px",
+              margin: "0 16px 0 0",
+              color: "rgba(87, 95, 117)",
+            }}
+          >
+            {format(new Date(date), "MM-dd")}
+          </p>
+          {title}
+        </div>
+      </Link>
     </li>
   );
 }
